67ccd527743626cc18e831393a746a9994cdef05
Yuhei Hara <haradayo623@gmail.com>
2024-01-26 18:52:09 +0900

    Update build tool

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.jsx"),
+      name: "react-hubspot-form",
+      fileName: "react-hubspot-form",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "React-dom",
+        },
+      },
+    },
+  },
+});
